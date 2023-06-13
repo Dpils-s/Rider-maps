cc1f855f575eb1872e8b56e92d14a3c219afd284
Artjoms Figurins <92020227+Otrigos@users.noreply.github.com>
2023-06-13 17:50:27 +0200

    Update npm

---
+++ src/js/AddressList.js
@@ -0,0 +1,32 @@
+import { getAllAddresses } from '@/DAL/AddressListDAL'; // Import the getAllAddresses function
+
+export default {
+    data() {
+        return {
+            addresses: [],
+        };
+    },
+    mounted() {
+        this.fetchAddresses();
+    },
+    methods: {
+        async fetchAddresses() {
+            try {
+                // Fetch the addresses using the getAllAddresses function
+                this.addresses = await getAllAddresses();
+            } catch (error) {
+                console.error('Error:', error);
+            }
+        },
+        handleButtonClick(address) {
+            // Convert the address object to a JSON string
+            const addressJSON = JSON.stringify(address);
+
+            // Store the address JSON in session storage
+            sessionStorage.setItem('address', addressJSON);
+
+            // Redirect to the RiderRoute page
+            this.$router.push('/rider-route');
+        },
+    },
+};
